73c2a33321d20944072eee70c6f11714699598cd
PaigeHudacko <50750553+PaigeHudacko@users.noreply.github.com>
2019-05-31 10:45:26 -0700

    Update drawing.js

--- drawing.js
+++ drawing.js
@@ -73,12 +73,7 @@ function keyPressed() {
         o+=15
     } else if(keyCode === 68 && o>0){
         o-=15
-    } else if(keyCode === 32){
-         noStroke();
-        fill(r, g ,b, o);
-    ellipse(mouseX, mouseY, s)
-    }
-
+    } 
 }
 function draw() {
 
