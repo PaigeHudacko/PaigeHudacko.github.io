e8cc5d02b2aa1c7b61aa2bf0414f75aa4e0e0d7b
PaigeHudacko <50750553+PaigeHudacko@users.noreply.github.com>
2019-05-31 10:09:59 -0700

    Create input.js

---
+++ input.js
@@ -0,0 +1,13 @@
+// Randomly generates a map based on variables below
+
+// true/false
+var island = false
+var island = true
+var forests = true
+var mountains = true
+
+
+// these change the chance/amount of land, forests, and mountains that will be created.
+var chanceOfLife = 0.19 // less = more land
+var chanceOfForest = 0.05 // more = more forest
+var chanceOfMountain = 0.08 // more = more mountains
